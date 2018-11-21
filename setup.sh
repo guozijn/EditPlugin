4d29d90978ce2e15bab0fe111b7f02a2b6822329
guozijian <guozijn@gmail.com>
2018-11-21 16:00:44 +0800

    Modified setup.sh
    Signed-off-by: guozijian <guozijn@gmail.com>

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 DIR_NAME=$(dirname "$REAL_PATH")
 yes | cp -rf $DIR_NAME/.vim* $HOME
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    echo "Install vundle ..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
