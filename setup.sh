0c30f66ddef6e53e00b849a592fde62d9fada9fe
guozijian <guozijn@gmail.com>
2018-11-21 18:19:28 +0800

    Modefied setup.sh
    Signed-off-by: guozijian <guozijn@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 DIR_NAME=$(dirname "$REAL_PATH")
+
 if [ ! -d $HOME/.vim ]; then
     mkdir $HOME/.vim
     echo "Install colors ..."
@@ -12,8 +13,12 @@ else
     echo "Directory already exist"
     exit 1
 fi
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Install vundle ..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
+    echo "Install vundle finished"
 fi
+
+echo "EditPlugin installed"
