459923fd03dd26f1f99b4f4378d5bfa27615c43d
guozijian <guozijn@gmail.com>
2018-11-21 18:01:27 +0800

    Refactor install

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 DIR_NAME=$(dirname "$REAL_PATH")
-if [ ! -d ~/.vim/ ]; then
+if [ ! -d $HOME/.vim ]; then
+    mkdir $HOME/.vim
     echo "Install plugins ..."
-    yes | cp -rf $DIR_NAME/.vim* $HOME
+    cp -rf $DIR_NAME/.vim/colors/ $HOME/.vim
+    echo "Install plugins finished"
 else
     echo "Directory already exist"
     exit 1
@@ -12,5 +14,5 @@ fi
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Install vundle ..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim "+PluginInstall" "+x" "+x"
+    vim +PluginInstall +qall
 fi
