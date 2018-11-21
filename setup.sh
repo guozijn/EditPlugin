f3d4cf1ee15618898abc2a6fa222ab20be1a3b63
guozijian <guozijn@gmail.com>
2018-11-21 16:12:48 +0800

    Add vundle install in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 DIR_NAME=$(dirname "$REAL_PATH")
-yes | cp -rf $DIR_NAME/.vim* $HOME
+if [ ! -d ~/.vim/ ]; then
+    echo "Install plugins ..."
+    yes | cp -rf $DIR_NAME/.vim* $HOME
+else
+    echo "Directory already exist"
+    exit 1
+fi
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Install vundle ..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
